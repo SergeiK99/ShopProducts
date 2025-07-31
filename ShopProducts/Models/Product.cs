262e335a75def0623a1ae74840fb6a84a5783514
Сергей Колесников <skx2013@mail.ru>
2025-07-31 16:58:50 +0700

    ver1

---
+++ ShopProducts/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopProducts.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImagePath { get; set; }
+        public double Price { get; set; }
+    }
+}
